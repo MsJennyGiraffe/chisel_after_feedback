82984a48ab2a648e9aa6e9886cb5086732f8d555
Jennifer <jennifersoden@hotmail.com>
2016-03-09 15:45:01 -0700

    still listing

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -9,6 +9,7 @@ class ConverterTest < Minitest::Test
   end
 
   def test_header_converter_takes_octothropes_and_turns_them_to_html_headers
+    skip
     assert_equal "<h1>hello</h1>", @converter.header_converter("#hello")
     assert_equal "<h2>hello</h2>", @converter.header_converter("##hello")
     assert_equal "<h3>hello</h3>", @converter.header_converter("###hello")
@@ -18,43 +19,33 @@ class ConverterTest < Minitest::Test
   end
 
   def test_header_converter_makes_paragraphs_tags_when_no_headers_are_found
+    skip
     assert_equal "<p>hello</p>", @converter.header_converter("hello")
   end
 
   def test_strong_returns_strong_tags_where_double_asterics_are_found
+    skip
     assert_equal "<strong>hello</strong>", @converter.strong("**hello**")
     assert_equal "<strong>hello</strong> <strong>you</strong>", @converter.strong("**hello** **you**")
   end
 
   def test_emphasis_returns_em_tags_where_single_asterics_are
+    skip
     assert_equal "<em>hello</em>", @converter.emphasis("*hello*")
   end
 
-
   def test_converted_contents_properly_formats_from_markdown_to_html
-  assert_equal "<p><strong>I am <em>so</strong></em> glad this is almost over.</p>", @converter.converted_contents
-  end
-
-#hardcode branches to test
-
-  def test_unordered_lists_insert_unordered_list_tags_before_and_after_list
     skip
-    helper = TestHelper.new
-    assert_equal helper.list_html, @converter.unordered_lists(helper.list_html, 0, 4)
+    assert_equal "<p><strong>I am <em>so</strong></em> glad this is almost over.</p>", @converter.converted_contents
   end
 
-  def test_unordered_lists_inserts_list_tags_in_place_of_asterics_and_inserts_ul_tags
-    skip
-    assert_equal "<ul><li>list</li>", @converter.unordered_lists("* list", 0, 1)
+  def test_unordered_list_tags_replace_asterics
+    assert_equal "<li> list </li>", @converter.unordered_list_tags_list("* list")
   end
 
-  def test_ordered_lists_insert_unordered_list_tags_before_and_after_list
-    skip
-    assert_equal "<ul><li>list</li>", @converter.unordered_lists("* list", 0, 0)
-  end
-
-  def test_ordered_lists_changes_list_items_from_numbers_to_ordered_list_tags
-
+  def test_unordered_list_tags_creates_unordered_list_opening_and_closing_notation
+    html = @converter.unordered_list_tags_list("* list")
+    assert_equal 0, unordered_list_tags(html)
   end
 
 end
